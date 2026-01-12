a6637c20db2f4d4038d154b4cb8d73d630c8e1f4
www1240kr-web <www.1240.kr@gmail.com>
2026-01-12 11:52:04 +0900

    setName 함수 사용하기

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,27 @@
-import { useState } from 'react';
+import React, { useState } from 'react';
 import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
 import './App.css';
 
 function App() {
   const [count, setCount] = useState(0);
+  // state값은 state 함수를 통해서만 변경해야한다.
+  // setState 함수 호출시 해당 컴포넌트가 재랜더링 된다.
+  const [name, setName] = useState('홍길동');
+
+  const handleName = () => {
+    setName('임꺽정');
+  };
+
+  const handleNameChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setName(event.target.value);
+  };
 
   return (
     <>
+      <p>{name}</p>
+      <button onClick={handleName}>이름 변경</button>
+      <input className="border p-1.5" type="text" onChange={handleNameChange} />
       <h1 className="text-3xl font-bold underline">Hello world!</h1>
       <div>
         <a href="https://vite.dev" target="_blank">
