b0c3d065193c931bd4543443d2006ed63f2fc585
www1240kr-web <www.1240.kr@gmail.com>
2026-01-12 14:45:19 +0900

    props drilling

--- src/App.tsx
+++ src/App.tsx
@@ -1,46 +1,38 @@
-import React, { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
+//import React, { useState } from 'react';
+import React from 'react';
+
 import './App.css';
-import { MyName } from './MyName';
+import { UserInfo } from './UserInfo';
 
 function App() {
-  const [count, setCount] = useState<number>(0);
   // state값은 state 함수를 통해서만 변경해야한다.
   // setState 함수 호출시 해당 컴포넌트가 재랜더링 된다.
-  const [name, setName] = useState<string>('홍길동');
-
-  const handleName = () => {
-    setName('임꺽정');
-  };
+  const [name, setName] = React.useState<string>('홍길동');
+  const [age, setAge] = React.useState<number>(20);
 
   const handleNameChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setName(event.target.value);
   };
 
+  const handleAgeChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setAge(Number(event.target.value));
+  };
+
   return (
     <>
-      <p>{name}</p>
-      <button onClick={handleName}>이름 변경</button>
-      <input className="border p-1.5" type="text" onChange={handleNameChange} />
-      <MyName name={name} />
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
+      <input
+        className="border p-1.5"
+        type="text"
+        placeholder="이름을 입력하세요."
+        onChange={handleNameChange}
+      />
+      <input
+        className="border p-1.5 ml-2"
+        type="number"
+        placeholder="나이를 입력하세요."
+        onChange={handleAgeChange}
+      />
+      <UserInfo name={name} age={age} />
     </>
   );
 }
---
+++ src/UserInfo1.tsx
@@ -0,0 +1,16 @@
+import { MyName } from './MyName';
+import { MyAge } from './MyAge';
+
+interface UserInfo1Props {
+  name: string;
+  age: number;
+}
+
+export function UserInfo1({ name, age }: UserInfo1Props) {
+  return (
+    <>
+      <MyName name={name} />
+      <MyAge age={age} />
+    </>
+  );
+}
